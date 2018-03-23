d83ddad7e8db3cc2a51b1ec850ecd6de58db2e84
CarlosCedillo <crloz.cedillo@gmail.com>
2018-03-22 22:11:32 -0700

    Clase fichas terminada

--- src/tablas/fichas.java
+++ src/tablas/fichas.java
@@ -1,14 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tablas;
 
-/**
- *
- * @author CaGeCeAl
- */
 public class fichas {
+    
+    private Integer id;
+    private String texto;
+    private Integer categoria;
+    private Integer subCategoia1;
+    private Integer subCategoia2;
+    private Integer subCategoia3;
+    private String fuente;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Integer getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Integer categoria) {
+        this.categoria = categoria;
+    }
+
+    public Integer getSubCategoia1() {
+        return subCategoia1;
+    }
+
+    public void setSubCategoia1(Integer subCategoia1) {
+        this.subCategoia1 = subCategoia1;
+    }
+
+    public Integer getSubCategoia2() {
+        return subCategoia2;
+    }
+
+    public void setSubCategoia2(Integer subCategoia2) {
+        this.subCategoia2 = subCategoia2;
+    }
+
+    public Integer getSubCategoia3() {
+        return subCategoia3;
+    }
+
+    public void setSubCategoia3(Integer subCategoia3) {
+        this.subCategoia3 = subCategoia3;
+    }
+
+    public String getFuente() {
+        return fuente;
+    }
+
+    public void setFuente(String fuente) {
+        this.fuente = fuente;
+    }
+    
+    
     
 }
