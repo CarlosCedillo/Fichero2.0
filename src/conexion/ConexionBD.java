3ebe49075e5710e917ac395ef10801ffafbb8718
CarlodCedillo <cgca_0223@hotmail.com>
2018-03-25 21:38:19 -0700

    Agrego las librerias

--- src/conexion/ConexionBD.java
+++ src/conexion/ConexionBD.java
@@ -8,7 +8,7 @@ import javax.swing.JOptionPane;
 public class ConexionBD {
     static Connection conexion = null;
     static String driver = "net.ucanaccess.jdbc.UcanaccessDriver";
-    static String url = "jdbc:ucanaccess://C:\\Users\\CaGeCeAl\\Documents\\NetBeansProjects\\Fichero2.0\\src\\BD\\fichero2.0.accdb";
+    static String url = "jdbc:ucanaccess://C:\\Users\\Carlos Cedillo\\Documents\\NetBeansProjects\\Fichero2.0\\src\\BD\\fichero2.0.accdb";
     //static String url = "jdbc:ucanaccess://C:\\Users\\CaGeCeAl\\Documents\\NetBeansProjects\\Fichero2.0\\src\\BD\\fichero2.0.accdb";
     
     public static Connection obtenerConexion() {
@@ -16,7 +16,7 @@ public class ConexionBD {
             if(conexion == null){
                 Class.forName(driver);
                 conexion = DriverManager.getConnection(url);
-                //JOptionPane.showMessageDialog(null, "Conexion establecida   ");
+                JOptionPane.showMessageDialog(null, "Conexion establecida   ");
             }
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -25,7 +25,7 @@ public class ConexionBD {
         return conexion;
     }
     
-//    public static void main(String[] args) {
-//        Connection cn = ConexionBD.obtenerConexion();
-//    }
+    public static void main(String[] args) {
+        Connection cn = ConexionBD.obtenerConexion();
+    }
 }
