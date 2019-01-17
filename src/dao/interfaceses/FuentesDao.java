c84eb9d506fcbb94886230439fea55b91447d0de
CarlosCedillo <cgca_0223@hotmail.com>
2019-01-16 18:42:43 -0700

    Metodo abstracto obtenerId declarado en FuenteDao, guardar y modificar comentados

--- src/dao/interfaceses/FuentesDao.java
+++ src/dao/interfaceses/FuentesDao.java
@@ -2,4 +2,8 @@ package dao.interfaceses;
 
 public interface FuentesDao {
     
+//    public boolean guardar(String nombre);
+    public Integer obtenerId(String nombre);
+//    public boolean modificar(Integer id, String nvoNombre);
+    
 }
