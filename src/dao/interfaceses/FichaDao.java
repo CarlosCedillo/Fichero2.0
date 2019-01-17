86800c475da17ad2ea6e5acd4121466fefaec43a
CarlosCedillo <cgca_0223@hotmail.com>
2019-01-16 18:39:52 -0700

    Metodo abstracto busquedaAvanzada declarado en FichaDao

--- src/dao/interfaceses/FichaDao.java
+++ src/dao/interfaceses/FichaDao.java
@@ -1,5 +1,9 @@
 package dao.interfaceses;
 
-public interface FichaDao {
+import java.sql.ResultSet;
 
+public interface FichaDao {
+    
+    public ResultSet busquedaAvanzada(Integer categoriaId, Integer sub1Id, Integer sub2Id, Integer sub3Id, Integer fuenteId) throws Exception;
+    
 }
