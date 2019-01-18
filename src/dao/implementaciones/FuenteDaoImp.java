fbeeaa4f0ae780adb90dca69ce7f07f1deb85551
CarlosCedillo <cgca_0223@hotmail.com>
2019-01-18 12:09:28 -0700

    FuenteDaiImpl completamente funcional

--- src/dao/implementaciones/FuenteDaoImp.java
+++ src/dao/implementaciones/FuenteDaoImp.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import tablas.Fuentes;
 
@@ -51,20 +52,139 @@ public class FuenteDaoImp extends ConexionBD implements FuentesDao{
         return fuenteId;
         
     }
+    
+    @Override
+    public String obtenerNombre(Integer id) {
+        
+        String fuenteNombre = null;
+        
+        Connection conexion;
+        PreparedStatement preparedStatement;
+        ResultSet resultSet;
+        
+        System.out.println("Ejecutando: SELECT nombre FROM fuentes WHERE id = '"+id+"'");
+        
+        try {
+            
+            conexion = ConexionBD.obtenerConexion();
+            
+            preparedStatement = conexion.prepareCall("SELECT nombre FROM fuentes WHERE id = ?");
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+            
+            while( resultSet.next() ){
+                Fuentes fuentes = new Fuentes();
+                fuentes.setId(resultSet.getInt("id"));
+                
+                fuenteNombre = fuentes.getNombre();
+                
+            }
+            
+            conexion = ConexionBD.cerrarConexion();
+            preparedStatement.close();
+            resultSet.close();
+            
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        return fuenteNombre;
+        
+    }
 
     @Override
-    public boolean guardar(Integer id, String nombre) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean guardar(String nombre) {
+        
+        boolean guardado = false;
+        
+        Connection conexion;
+        PreparedStatement preparedStatement;
+        
+        System.out.println("\nEjecutando: INSERT INTO fuentes (nombre) VALUES (?)");
+        
+        try {
+                
+            conexion = ConexionBD.obtenerConexion();
+            
+            preparedStatement = conexion.prepareStatement("INSERT INTO fuentes (nombre) VALUES (?)");
+            preparedStatement.setString(1, nombre);
+            preparedStatement.executeUpdate();
+                
+            guardado = true;
+                
+            conexion = ConexionBD.cerrarConexion();
+            preparedStatement.close();
+                
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        return guardado;
+        
     }
 
     @Override
     public List<Fuentes> listar() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        Connection conexion = null;
+        List<Fuentes> listaFuentes = null;
+        listaFuentes = new ArrayList<>();
+        
+        try {
+            conexion = ConexionBD.obtenerConexion();
+            ResultSet resultSet;
+            String sql = "SELECT * FROM fuentes ORDER BY nombre ASC;";
+            PreparedStatement preparedStatement = conexion.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+            
+            while (resultSet.next()) {
+                Fuentes fuentes = new Fuentes();
+                fuentes.setId(resultSet.getInt("id"));
+                fuentes.setNombre(resultSet.getString("nombre"));
+                listaFuentes.add(fuentes);
+            }
+            
+            conexion = ConexionBD.cerrarConexion();
+            preparedStatement.close();
+            resultSet.close();
+            
+        } catch (SQLException ex) {
+            System.err.println(ex.getErrorCode());
+        }
+        
+        return listaFuentes;
+        
     }
 
     @Override
     public boolean modificar(Integer id, String nvoNombre) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        boolean modificado = false;
+        
+        Connection conexion;
+        PreparedStatement preparedStatement;
+        
+        System.out.println("\nEjecutando: UPDATE fuentes SET nombre = '"+nvoNombre+"' WHERE id = "+id);
+        
+        try {
+            
+            conexion = ConexionBD.obtenerConexion();
+            preparedStatement = conexion.prepareStatement("UPDATE fuentes SET nombre = ? WHERE id = ?");
+            preparedStatement.setString(1, nvoNombre);
+            preparedStatement.setInt(2, id);
+            preparedStatement.executeUpdate();
+
+            modificado = true;
+
+            conexion = ConexionBD.cerrarConexion();
+            preparedStatement.close();
+
+            } catch (SQLException | ClassNotFoundException ex) {
+                System.out.println(ex.getMessage());
+        }
+        
+        return modificado;
+        
     }
-    
+
 }
