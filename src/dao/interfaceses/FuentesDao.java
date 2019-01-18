3d02a1b04f09dbd7afe170a6cc5fbe9fafc1cd6a
CarlosCedillo <cgca_0223@hotmail.com>
2019-01-18 11:37:43 -0700

    daoFuentes.obtenerNombre declarado e implementado sin funcionar

--- src/dao/interfaceses/FuentesDao.java
+++ src/dao/interfaceses/FuentesDao.java
@@ -7,6 +7,8 @@ public interface FuentesDao {
     
     public Integer obtenerId(String nombre);
     
+    public String obtenerNombre(Integer id);
+    
     //CRYD (Create, Read, Update, Delete)
     
     public boolean guardar(Integer id, String nombre);
